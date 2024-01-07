b4d4f48d6ae1f726e899ef4be0e77e163bf384f1
maki40681 <maki4002681@gmail.com>
2024-01-07 08:27:58 +0530

    use kernel and initrd straight from root part removed efistub entry from install script

--- xtra/DEBchrootInstall/debootstrap.sh
+++ xtra/DEBchrootInstall/debootstrap.sh
@@ -22,13 +22,11 @@ dpkg-reconfigure tzdata
 printf "debian" > /etc/hostname
 sed -i '1s/$/ debian/' /etc/hosts
 
-cp /vmlinuz /boot/efi
-cp /initrd.img /boot/efi
-efibootmgr -c -g -L "Debian" -l '\vmlinuz' -u "root=UUID=$(/sbin/blkid -s UUID -o value /dev/sda2) \
-  rw quiet rootfstype=ext4 add_efi_memmap loglevel=0 udev.log_level=0 rd.systemd.show_status=false initrd=initrd.img"
+/usr/sbin/e2label /dev/sda2 debian
 printf "
 menuentry \"Debian\" {
         icon     /EFI/refind/icons/os_debian.png
+	volume	 \"debian\"
         loader   /vmlinuz
         initrd   /initrd.img
         options  \"root=PARTUUID=$(/sbin/blkid -s PARTUUID -o value /dev/sda2) rw quiet rootfstype=ext4 add_efi_memmap loglevel=0 udev.log_level=0 rd.systemd.show_status=false\"
@@ -38,8 +36,8 @@ umount -R /boot/efi
 printf "\nROOT Password\n"
 passwd
 
-useradd -m -G sudo,audio,video -s /usr/bin/zsh triplek
+useradd -m -G sudo,audio,video -s /usr/bin/zsh maki40681
 printf "\nUSER Password\n"
-passwd triplek
+passwd maki40681
 
 apt clean
